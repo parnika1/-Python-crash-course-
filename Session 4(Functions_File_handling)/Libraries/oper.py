b39103d4fd031b77633fd456fd0912ddf43d1602
parnika1 <parnika.pancholi@gmail.com>
2022-02-11 16:35:36 +0530

    Add files via upload

---
+++ Session 4(Functions_File_handling)/Libraries/oper.py
@@ -0,0 +1,35 @@
+# Python Module operations
+def add(a, b):
+    """
+    This program adds two
+    numbers and return the result
+    """
+    result = a + b
+    return result
+
+
+def sub(a, b):
+    """
+    This program adds two
+    numbers and return the result
+    """
+    result = a - b
+    return result
+
+
+def mul(a, b):
+    """
+    This program adds two
+    numbers and return the result
+    """
+    result = a * b
+    return result
+
+
+def mod(a, b):
+    """
+    This program adds two
+    numbers and return the result
+    """
+    result = a % b
+    return result
